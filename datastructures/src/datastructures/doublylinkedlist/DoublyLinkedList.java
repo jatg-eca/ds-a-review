ad58c45c5d66911b9885b68cae200a641aee2fa5
JA Gonzalez <jatgonzalez33@gmail.com>
2022-09-13 20:52:27 -0500

    Add: Append method O(n)

--- datastructures/src/datastructures/doublylinkedlist/DoublyLinkedList.java
+++ datastructures/src/datastructures/doublylinkedlist/DoublyLinkedList.java
@@ -22,25 +22,50 @@ public class DoublyLinkedList {
 		length = 1;
 	}
 	
+	public DoublyLinkedList() {}
+	
 	public void printList() {
 		Node temp = head;
 		while(temp != null) {
-			int index = 0;
-			System.out.println("Index " + index + " = " + temp.value);
+			int indexCount = 0;
+			System.out.println("Index " + indexCount + " = " + temp.value);
 			temp = temp.next;
+			indexCount ++;
 		}
 	}
 	
 	public void getHead() {
-		System.out.println("Head: " + this.head.value);
+		if(head == null) System.out.println("No head :(");
+		else System.out.println("Head: " + this.head.value);
 	}
 	
 	public void getTail() {
-		System.out.println("Tail: " + this.tail.value);
+		if(tail == null) System.out.println("Tail: null");
+		else System.out.println("Tail: " + this.tail.value);
 	}
 	
 	public void getLength() {
 		System.out.println("Length: " + this.length);
+	}
+	
+	public void append(int value) {
+		Node newNode = new Node(value);
+		Node prev = null;
+		Node temp = this.head;
+		if(length <1) {
+			head = newNode;
+			tail = newNode;
+			length = 1;
+		} else {
+		while(temp.next != null) {
+			prev = temp;
+			temp = temp.next;
+		}
+		temp.next = newNode;
+		newNode.prev = prev;
+		tail = newNode;
+		length ++;
+		}
 	}
 
 }
--- datastructures/src/datastructures/doublylinkedlist/MainDLL.java
+++ datastructures/src/datastructures/doublylinkedlist/MainDLL.java
@@ -4,11 +4,17 @@ public class MainDLL {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DoublyLinkedList myDll = new DoublyLinkedList(7);
+		DoublyLinkedList myDll = new DoublyLinkedList();
 		
 		myDll.getHead();
 		myDll.getTail();
 		myDll.getLength();
+		
+		myDll.printList();
+		
+		myDll.append(7);
+		myDll.append(9);
+		myDll.append(16);
 		myDll.printList();
 
 	}
