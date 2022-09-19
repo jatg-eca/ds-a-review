b15d3ade2be09d4422b022a45df252e3e2a973e5
JA Gonzalez <jatgonzalez33@gmail.com>
2022-09-18 20:46:47 -0500

    Add; BST advances

--- datastructures/src/datastructures/binarysearchtree/BinarySearchTree.java
+++ datastructures/src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package datastructures.binarysearchtree;
 
+import org.junit.jupiter.api.io.TempDir;
+
 public class BinarySearchTree {
 	private Node root;
 	
@@ -24,6 +26,38 @@ public class BinarySearchTree {
 		return root;
 	}
 	
-	
+	public boolean insert(int value) {
+		Node newNode = new Node(value);
+		boolean achieved = false;
+		if(root == null) {
+			root = newNode;
+			achieved = true;
+		} else {
+		Node temp = root;
+		while(temp != null) {
+			if(newNode.value == temp.value) {
+				break;
+			}
+			if(newNode.value < temp.value) {
+				System.out.println("menor");
+				temp = temp.left;
+			} 
+			else if(newNode.value > temp.value) {
+				System.out.println("mayor");
+				System.out.println("Temp antes" + temp.value);
+				temp = temp.right;
+				System.out.println("Temp después" + temp);
+			}
+			if(temp == null) {
+				achieved = true;
+				System.out.println("ok");
+				temp = newNode;
+			}
+			else { 
+				continue;
+				}
+		}}
+		return achieved;
+	}
 
 }
--- datastructures/src/datastructures/binarysearchtree/MainBst.java
+++ datastructures/src/datastructures/binarysearchtree/MainBst.java
@@ -8,7 +8,12 @@ public class MainBst {
 		BinarySearchTree mybts = new BinarySearchTree();
 		//System.out.println("Root = " +  mybts.root);
 		System.out.println(mybts.getRoot());
-		
+		System.out.println(mybts.insert(54));
+		System.out.println(mybts.insert(54));
+		System.out.println(mybts.insert(56));
+		System.out.println(mybts.insert(25));
+		System.out.println(mybts.insert(84));
+		System.out.println(mybts.insert(36));
 		
 	}
 
