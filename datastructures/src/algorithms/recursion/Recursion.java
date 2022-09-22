ed6ed186b05106b83ca1d182ea447738e56d24bb
JA Gonzalez <jatgonzalez33@gmail.com>
2022-09-22 13:08:58 -0500

    Add: Intro to recursion w.pseudocode

---
+++ datastructures/src/algorithms/recursion/Recursion.java
@@ -0,0 +1,27 @@
+package algorithms.recursion;
+
+public class Recursion {
+	
+	
+	
+//	/**
+//	 * Recursion method
+//	 * Continues happening until it doesn't:
+//	 * a ball is found after the big box
+//	 * gets more and more little, like matrioshkas do
+//	 * @return ball
+//	 */
+//	public static boolean openTheBox() {
+//		boolean ball = true;
+//		if(ballIsThere) {
+//			return ball;
+//		}
+//		openTheBox();
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
