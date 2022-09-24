5f9e2e86e69f6962a838b88557a91c3f2f089699
JA Gonzalez <jatgonzalez33@gmail.com>
2022-09-24 17:47:49 -0500

    Add: Pivot and swap methods. Refactorized

--- datastructures/src/algorithms/sort/quicksort/QuickSort.java
+++ datastructures/src/algorithms/sort/quicksort/QuickSort.java
@@ -4,6 +4,32 @@ import java.util.Arrays;
 
 public class QuickSort {
 	
+	public static int pivoting(int [] array, int pivotIndex, int endingIndex ) {
+		int swapIndex = pivotIndex;
+		for(int i=pivotIndex+1; i<endingIndex; i++) {
+			if(array[i] < array[pivotIndex]) {
+				swapIndex++;
+//				int temp = array[swapIndex];
+//				array[swapIndex] = array[i];
+//				array[i] = temp;
+				swap(array, swapIndex, i);
+			}
+		}
+//		int temp = array[swapIndex];
+//		array[swapIndex] = array[pivotIndex];
+//		array[pivotIndex] = temp;
+		swap(array, swapIndex, pivotIndex);
+		
+		System.out.println( Arrays.toString( array ));
+		return swapIndex;
+	}
+	
+	public static void swap(int [] array, int swapIndex, int infoIndex) {
+		int temp = array[swapIndex];
+		array[swapIndex] = array[infoIndex];
+		array[infoIndex] = temp;
+	}
+	
 	/***
 	 * La función de pivote
 	 * ordena un array de un lado los menores a
@@ -13,7 +39,7 @@ public class QuickSort {
 	 * @param array
 	 * @return
 	 */
-	public static int pivoting(int [] array) {
+	public static int pivotingOne(int [] array) {
 		int pivot = 0;
 		int swap = pivot;
 		for(int i=1; i<array.length; i++) {
@@ -36,10 +62,11 @@ public class QuickSort {
 	}
 	
 	
+	
 	public static void main(String[] miau) {
 		int [] myarray = {4, 6, 1, 7, 3, 2, 5};
 //		int [] myarray = {9, 6, 1, 7, 3, 2, 5, 8, 4};
-		System.out.println(pivoting(myarray));
+		System.out.println(pivoting(myarray, 0, myarray.length));
 	}
 
 }
